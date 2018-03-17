aed5e4c86519150855bd7d35a805c9eb02093b9f
Shaun Tucker <tucke@shauns-air.home>
2018-03-16 20:16:10 -0400

    label added

--- facebook/FeedController.swift
+++ facebook/FeedController.swift
@@ -17,12 +17,68 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         navigationItem.title = "Futucke Inc"
         
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellId)
+        
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
         
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
 
- 
+   
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
 
 
 }
+
+class FeedCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Futucke Inc"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    func setupViews() {
+        
+        backgroundColor = UIColor.white
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
